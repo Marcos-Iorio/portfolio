98b3652820ba275335425ed7fbd83a46e09198a9
Marcos Iorio <marcossiorio@gmail.com>
2022-10-20 20:34:20 -0300

    fixing some errors and updating projects and stack

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,57 +1,90 @@
-import React, {useState, useCallback} from 'react'
-import Scene from '../scene/Scene';
+import React, { useState, useCallback } from "react";
+import Scene from "../scene/Scene";
 
-import styles from './About.module.scss'
+import styles from "./About.module.scss";
 
 import { SiGithub, SiLinkedin } from "react-icons/si";
-import Button from '../Button/Button';
-import Modal from '../Modal/Modal';
-import { useThemeContext } from '../../hooks/useThemeContext';
-
+import Button from "../Button/Button";
+import Modal from "../Modal/Modal";
+import { useThemeContext } from "../../hooks/useThemeContext";
 
 const About = () => {
-    const [modal, setModal] = useState(false);
+  const [modal, setModal] = useState(false);
 
-    const {isDark} = useThemeContext();
+  const { isDark } = useThemeContext();
 
-    const openModalHandler = () => {
-        setModal(prev => !prev)
-    }
-    
-    const closeModalHandler = () => {
-        setModal(false)
-    }
+  const openModalHandler = () => {
+    setModal((prev) => !prev);
+  };
 
-    return(
-        <section className={`${styles.about__wrapper} ${isDark ? styles['light-theme'] : styles['dark-theme']}`} id="about">
-            <div className={styles.about__content}>
-                <div className={styles.about__info}>
-                    <h3 className={styles.about__title}>About me.</h3>
-                    <p className={styles.about__description}>
-                    I’m from <b>Buenos Aires, Argentina.</b><br/>
-                    I’m working with Wordpress since 2021.<br/>
-                    My meta is to learn new technologies and become a FullStack developer.<br/>
-                    I code in JavaScript, React, Sass, Tailwind.<br/>
-                    I also have knowledge of Node with Express in the backend.<br/>
-                    I would love to learn Three js and Blender to make amazing things in the web.<br/>
-                    </p>
-                    <div className={styles.download_cv}>
-                        <Button onClick={openModalHandler} type='cv' buttonText='Download CV'/>
-                        <img className={styles.button__detail} src="./Button.svg" alt="Button detail" />
-                    </div>
-                </div>
-                <div className={styles.about__model}>
-                    <img className={styles.three__detail} src="./three.svg" alt="Treejs detail" />
-                    <Scene/>
-                    <div className={styles.platforms__links}>
-                        <a href="https://github.com/Marcos-Iorio" target='_blank'><SiGithub/></a>
-                        <a href="https://www.linkedin.com/in/marcos-ignacio-iorio/" target='_blank'><SiLinkedin/></a>
-                    </div>
-                </div>
-            </div>
-            {modal && <Modal onConfirm={closeModalHandler}/>}
-        </section>
-    );
-}
+  const closeModalHandler = () => {
+    setModal(false);
+  };
+
+  return (
+    <section
+      className={`${styles.about__wrapper} ${
+        isDark ? styles["light-theme"] : styles["dark-theme"]
+      }`}
+      id="about"
+    >
+      <div className={styles.about__content}>
+        <div className={styles.about__info}>
+          <h3 className={styles.about__title}>About me.</h3>
+          <p className={styles.about__description}>
+            I’m from <b>Buenos Aires, Argentina.</b>
+            <br />
+            I’m working with Wordpress since 2021.
+            <br />
+            My goal is to learn new technologies and become a FullStack
+            developer.
+            <br />
+            I code in JavaScript, React, Sass, Tailwind.
+            <br />
+            I also have knowledge of Node with Express in the backend.
+            <br />
+            I would love to learn Three js and Blender to make amazing things in
+            the web.
+            <br />
+            I'm currently practicing Nextjs :)
+            <br />
+          </p>
+          <div className={styles.download_cv}>
+            <Button
+              onClick={openModalHandler}
+              type="cv"
+              buttonText="Download CV"
+            />
+            <img
+              className={styles.button__detail}
+              src="./Button.svg"
+              alt="Button detail"
+            />
+          </div>
+        </div>
+        <div className={styles.about__model}>
+          <img
+            className={styles.three__detail}
+            src="./three.svg"
+            alt="Treejs detail"
+          />
+          <Scene />
+          <div className={styles.platforms__links}>
+            <a href="https://github.com/Marcos-Iorio" target="_blank">
+              <SiGithub />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/marcos-ignacio-iorio/"
+              target="_blank"
+            >
+              <SiLinkedin />
+            </a>
+          </div>
+        </div>
+      </div>
+      {modal && <Modal onConfirm={closeModalHandler} />}
+    </section>
+  );
+};
 
 export default About;
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,49 +1,94 @@
-import React from 'react'
+import React from "react";
 
-import styles from './Projects.module.scss';
+import styles from "./Projects.module.scss";
 import { SiGithub, SiLinkedin } from "react-icons/si";
 import { MdOutlineOpenInNew } from "react-icons/md";
 
-import { projects } from '/src/data.js'
-import { v4 as uuidv4 } from 'uuid';
-import Slider, {SliderItem} from '../Slider/Slider';
-import {useThemeContext} from '../../hooks/useThemeContext'
-
+import { projects } from "/src/data.js";
+import { v4 as uuidv4 } from "uuid";
+import Slider, { SliderItem } from "../Slider/Slider";
+import { useThemeContext } from "../../hooks/useThemeContext";
 
 const Projects = () => {
+  const { isDark } = useThemeContext();
 
-    const {isDark} = useThemeContext()
-
-    return(
-        <section className={`${styles.project__wrapper} ${isDark ? styles['dark-theme'] : styles['light-theme']}`} id='projects'>
-            <div className={styles.project__content}>
-                <h2 className={styles.project__title}>Projects.</h2>
-                <img className={styles.font__detail} src="./font.svg" alt="Font family type" />
-                <Slider>
-                {
-                    projects.map(project => {
-                        return <SliderItem key={uuidv4()}>
-                                    <article className={`${styles.project} ${isDark ? styles['dark-project'] : styles['light-project']}`}>
-                                        <div className={styles.header}>
-                                            <h3 className={`${styles.title} ${isDark ? styles['dark-text'] : styles['light-text']}`}>{project.name}</h3>
-                                            {project.github !== null ?  <a href={project.github} target='_blank' className={styles.project__repo}><SiGithub className={`${isDark ? styles['dark-text'] : styles['light-text']}`}/></a> : ''}
-                                        </div>
-                                        <p className={`${styles.project__description} ${isDark ? styles['dark-text'] : styles['light-text']}`}>
-                                            {project.description}
-                                        </p>
-                                        <div className={styles.project_link_wrapper}>
-                                            {project.livePage !== null ? <a href={project.livePage} target="_blank" className={`${styles.project__link} ${isDark ? styles['dark-text'] : styles['light-text']}`}>View Project <MdOutlineOpenInNew/></a> : '' }
-                                        </div>
-                                    </article>
-                                </SliderItem>
-
-                    })
-                }
-                </Slider>
-            </div>
-
-        </section>
-    );
-}
+  return (
+    <section
+      className={`${styles.project__wrapper} ${
+        isDark ? styles["dark-theme"] : styles["light-theme"]
+      }`}
+      id="projects"
+    >
+      <div className={styles.project__content}>
+        <h2 className={styles.project__title}>Projects.</h2>
+        <img
+          className={styles.font__detail}
+          src="./font.svg"
+          alt="Font family type"
+        />
+        <Slider>
+          {projects.map((project) => {
+            return (
+              <SliderItem key={uuidv4()}>
+                <article
+                  className={`${styles.project} ${
+                    isDark ? styles["dark-project"] : styles["light-project"]
+                  }`}
+                >
+                  <div className={styles.header}>
+                    <h3
+                      className={`${styles.title} ${
+                        isDark ? styles["dark-text"] : styles["light-text"]
+                      }`}
+                    >
+                      {project.name}
+                    </h3>
+                    {project.github !== null ? (
+                      <a
+                        href={project.github}
+                        target="_blank"
+                        className={styles.project__repo}
+                      >
+                        <SiGithub
+                          className={`${
+                            isDark ? styles["dark-text"] : styles["light-text"]
+                          }`}
+                        />
+                      </a>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                  <p
+                    className={`${styles.project__description} ${
+                      isDark ? styles["dark-text"] : styles["light-text"]
+                    }`}
+                  >
+                    {project.description}
+                  </p>
+                  <div className={styles.project_link_wrapper}>
+                    {project.livePage !== null ? (
+                      <a
+                        href={project.livePage}
+                        target="_blank"
+                        className={`${styles.project__link} ${
+                          isDark ? styles["dark-text"] : styles["light-text"]
+                        }`}
+                      >
+                        View Project <MdOutlineOpenInNew />
+                      </a>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                </article>
+              </SliderItem>
+            );
+          })}
+        </Slider>
+      </div>
+    </section>
+  );
+};
 
 export default Projects;
